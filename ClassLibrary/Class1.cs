6b0c5c6a6dfbb596d1c69cda8743dc417b76d635
alexey-leonovich <157357768+alexey-leonovich@users.noreply.github.com>
2024-07-10 15:53:12 +0300

    Add files via upload

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SQLite;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        private static string connectionString = new SQLiteConnectionStringBuilder { DataSource = "test.db" }.ToString();
+
+        public static void TestConnectionFTS5Init()
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                connection.EnableExtensions(true);
+                connection.LoadExtension("SQLite.Interop.dll", "sqlite3_fts5_init");
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ TestAppNFW/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary;
+using System;
+
+namespace TestAppNFW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.TestConnectionFTS5Init();
+            Console.WriteLine("TestConnectionFTS5Init - OK!");
+        }
+    }
+}
